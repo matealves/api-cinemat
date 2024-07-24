c6cd6883a2286f0018f351fffa6d406dda8c37d6
Mateus Alves <contatomateusalves@hotmail.com>
2024-07-24 16:41:49 -0300

    fix: add timestamps in model

--- api/src/models/User.ts
+++ api/src/models/User.ts
@@ -1,12 +1,17 @@
 import { Schema, model, Model, connection } from "mongoose";
 import { UserType } from "../types/user";
 
-const schema = new Schema<UserType>({
-  name: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-});
+const schema = new Schema<UserType>(
+  {
+    name: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export default connection && connection.models["User"]
   ? (connection.models["User"] as Model<UserType>)
