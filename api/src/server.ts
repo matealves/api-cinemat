058ca69945dde0e7ad6ff848edb0158c27c37184
Mateus Alves <contatomateusalves@hotmail.com>
2024-07-24 16:41:00 -0300

    fix: server

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,19 +1,24 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 
 import connectDB from "./database/mongodb";
-import router from "./routes/routes";
+import routes from "./routes/routes";
 
 dotenv.config();
 
 const server = express();
 const PORT = process.env.PORT || 4000;
 
-server.use("/", router);
+server.use(express.static(path.join(__dirname, "public")));
+server.use(express.json());
+
+server.use("/", routes);
+
 
 server.listen(PORT, () => {
   console.log(`[PORT:${PORT}] \x1b[32mServidor local iniciado.\x1b[0m\n`);
 });
 
 console.log("Conectando ao MongoDB...");
-// connectDB();
+connectDB();
