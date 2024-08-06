4e8efe70c251f11dc8ecc9ae221fb6ab09c460f7
Mateus Alves <contatomateusalves@hotmail.com>
2024-08-06 18:52:03 -0300

    feat: route shopping and Auth for private routes

--- api/src/routes/movies.routes.ts
+++ api/src/routes/movies.routes.ts
@@ -6,15 +6,18 @@ import * as MovieController from "../controllers/movieController";
 const router = Router();
 
 router.post("/", MovieController.create);
-// router.post("/seats/:id", Auth.private, MovieController.seats);
-router.post("/seats/:id", MovieController.seats);
+router.post("/seats/:id", Auth.private, MovieController.seats);
+// router.post("/seats/:id", MovieController.seats);
 
 router.get("/", MovieController.list);
 router.get("/:id", MovieController.getOne);
 router.get("/schedules/:id", MovieController.schedules);
+router.get("/shopping/:email", Auth.private, MovieController.shopping);
+// router.get("/shopping/:email", MovieController.shopping);
 
 router.put("/:id", MovieController.update);
-router.put("/tickets/:id", MovieController.buyTickets);
+router.put("/tickets/:id", Auth.private, MovieController.buyTickets);
+// router.put("/tickets/:id", MovieController.buyTickets);
 
 router.delete("/:id", MovieController.remove);
 
